9873ef3f7d73142914833ac375242da564cd4fab
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-11-09 21:42:38 -0400

    init gallery microservice

---
+++ gallery-service/src/test/java/js/entertainment/web/galleryservice/GalleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package js.entertainment.web.galleryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
